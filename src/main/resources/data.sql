3958c41553a8dba8eac7d3ab838bf3eaa2f85dfc
Thiago Adelino de Melo <thiagoadelinodemelo@gmail.com>
2024-07-01 23:19:53 +0200

    - updating the SQL inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,23 @@
 INSERT INTO author (id_author, name) VALUES
-  (1, 'João'),
-  (2, 'Felipe'),
-  (3, 'Lucas'),
-  (4, 'Fernando'),
-  (5, 'Francisco'),
-  (6, 'Mateus'),
-  (7, 'Romeo'),
-  (8, 'Juliano');
+  (1, 'Miguel de Cervantes'),
+  (2, 'George Orwell'),
+  (3, 'Leo Tolstoy'),
+  (4, 'J.K. Rowling'),
+  (5, 'Mark Twain');
 
 INSERT INTO book (id_book, name, id_author) VALUES
-  (1, 'E o vento levou', 1),
-  (2, 'O Alquimista', 2),
-  (3, 'Sensação', 3),
-  (4, 'Os papas', 4),
-  (5, 'Sem volta', 5),
-  (6, 'O vendedor de sonhos', 6),
-  (7, 'O fim', 7),
-  (8, 'Brás cubas', 8),
-  (9, 'O retorno', 1);
+  (1, 'Don Quixote', 1),
+  (2, '1984', 2),
+  (3, 'Animal Farm', 2),
+  (4, 'Homage to Catalonia', 2),
+  (5, 'War and Peace', 3),
+  (6, 'Anna Karenina', 3),
+  (7, 'The Death of Ivan Ilyich', 3),
+  (8, 'Harry Potter and the Philosophers Stone', 4),
+  (9, 'Harry Potter and the Chamber of Secrets', 4),
+  (10, 'Harry Potter and the Prisoner of Azkaban', 4),
+  (11, 'The Adventures of Tom Sawyer', 5),
+  (12, 'Adventures of Huckleberry Finn', 5),
+  (13, 'The Prince and the Pauper', 5);
+
+
